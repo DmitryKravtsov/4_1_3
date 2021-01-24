73f390a0349d939e4e1818d10c075d397001d3b3
Dmitry.P.Kravtsov <dmitry.p.kravtsov@gmail.com>
2021-01-24 13:29:34 +0300

    Third commit

--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,7 @@ public class Main {
         anywareTank.printPosition();
         customTank.goForward(201);
         customTank.printPosition();
+        customTank.printPosition();
+        customTank.printPosition();
     }
 }
